ba4a7ce531c3fd76d42bff769af2bb56b12d5fb6
Christian <cdarroch1210@gmail.com>
2022-09-12 11:09:05 -0400

    kn

---
+++ Strings hmwk.py
@@ -0,0 +1,49 @@
+#exercise 1
+# def any_lowercase1(s):
+#     for c in s:
+#         if c.islower():
+#             return True
+#         else:
+#             return False
+# if c is a lower case it returns true or false
+# def any_lowercase2(s):
+#     for c in s:
+#         if 'c'.islower():
+#             return 'True'
+#         else:
+#             return 'False'
+# it wouldnt work
+# def any_lowercase3(s):
+#     for c in s:
+#         flag = c.islower()
+#     return flag
+# it would only do the first value
+# def any_lowercase4(s):
+#     flag = False
+#     for c in s:
+#         flag = flag or c.islower()
+#     return flag
+#wouldnt work because when reran it would always be false
+# def any_lowercase5(s):
+#     for c in s:
+#         if not c.islower():
+#             return False
+#     return True
+#would work
+# def exercise2(a):
+#     revword= ""
+#     for i in range(len(a)-1,-1,-1):
+#         revword += a[i]
+#     return revword
+#
+# a = input("enter a word to see if iys a palindrome")
+# if exercise2(a) == a:
+#     print("yes it is a palindrome")
+# else: print("nope")
+def exercise3(a,b):
+    for i in range(0,len(a),1):
+        
+
+
+a = input("pls enter a word to rotate ")
+b =  int(input("pls enter a nuber of the amount of times you would like to rotate it over a number"))
---
+++ testing stuff.py
@@ -0,0 +1,6 @@
+value = ord("a")
+
+value1 = ord('b')
+
+# prints the unicode value
+print(value, value1)
