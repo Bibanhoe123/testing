fe15be957a8be4abc74d4ef64eafbdd756e87e62
Christian <cdarroch1210@gmail.com>
2022-09-15 11:09:36 -0400

    kn

--- Dictionaryhmwk.py
+++ Dictionaryhmwk.py
@@ -1,11 +1,31 @@
-import random
-def exercise1(adict):
-    for i in range:
-        x = random.randint(0,9)
-        adict[x]
-        print(x)
-randdict = { }
-exercise1(randdict)
+# import random
+# #fix later
+# def exercise1(adict):
+#     randomnums = []
+#     counter = 0
+#     countrandnums=[]
+#     keys=range(9)
+#     for i in range(1000):
+#         randomnums.append(random.randint(0,9))
+#     #print(x)
+#     for i in range(0,9):
+#         counter+=1
+#         countrandnums.append(randomnums.count(counter)/1000*100)
+#     counter=0
+#     for i in keys:
+#         counter+=1
+#         adict[i+1] = countrandnums[i]
+#         print(f'num {counter} ,chosen {adict[i+1]:.1f}%  time')
+#
+#
+# randdict = {}
+# exercise1(randdict)
+def exercise2(dict):
+    date = ""
+
+
+
+dict{}
 
 # dicts = {}
 # keys = range(4)
